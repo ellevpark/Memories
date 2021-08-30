dca1250edd61359e823f43dc4d98218401a4a0b4
Ellen Park <ellenpark781@gmail.com>
2021-08-30 13:57:03 -0400

    created title header and included image of a photograph

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.png'
 const App = () => {
     return(
-        <div>Hello World</div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"/>
+            </AppBar>
+        </Container>
     )
 }
 
