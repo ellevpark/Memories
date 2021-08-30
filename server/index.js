9befacfc5d8afd6378e351f46af210b360097e9f
Ellen Park <ellenpark781@gmail.com>
2021-08-30 13:27:47 -0400

    created controller & model for posts

--- server/index.js
+++ server/index.js
@@ -3,3 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
+const app = express(); 
+
+//middleware
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: '30mb', extended:true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin123@cluster0.ozzwg.mongodb.net/Memories?retryWrites=true&w=majority'
+// https://www.mongodb.com/cloud/atlas
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))
+    })
+    .catch((error) => console.log(error.message));
+
