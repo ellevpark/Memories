f1074146d527bbade1875ac0de88df6847496e88
Ellen Park <ellenpark781@gmail.com>
2021-08-30 14:40:51 -0400

    imported style sheet

---
+++ client/src/components/Posts/posts.js
@@ -0,0 +1,15 @@
+import Post from './Post/Post.js'
+
+const Posts = () => {
+    return (
+        <>
+        <div>
+            <h1>Posts</h1>
+            <Post/>
+            <Post/>
+        </div>
+        </>
+    )
+}
+
+export default Posts
