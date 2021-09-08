832ceb595c9c52495d653277dec64352d3fcb855
Ellen Park <ellenpark781@gmail.com>
2021-09-08 11:59:24 -0400

    created form using materialiu

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,10 +1,14 @@
+import {TextField, Button, Typography, Paper} from '@material-ui/core';
 import useStyles from './styles.js';
 
 
 const Form = () => {
     const classes = useStyles();
     return (
-        <h1>FORM</h1>
+        <Paper className = {classes.paper}>
+
+            <form autoComplete = "off" noValidate className = {classes.form} onSubmit = {handleSubmit}></form>
+        </Paper>
     )
 }
 
