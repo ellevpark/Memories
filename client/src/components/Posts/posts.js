317e109c65019980e1e9d331ef8c83cefe7a3951
Ellen Park <ellenpark781@gmail.com>
2021-09-08 11:34:40 -0400

    created hook for posts

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -1,7 +1,10 @@
 import Post from './Post/Post.js';
 import useStyles from "./styles.js";
+import {useSelector} from 'react-redux'
 const Posts = () => {
     const classes = useStyles();
+    const posts = useSelector(state => state.posts)
+    console.log(posts)
     return (
         <>
         <div>
