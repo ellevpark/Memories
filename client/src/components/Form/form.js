d7028983f80d3602ff0e65ed2652d98bca95b625
Ellen Park <ellenpark781@gmail.com>
2021-09-08 12:14:15 -0400

    created post form state

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,8 +1,17 @@
 import {TextField, Button, Typography, Paper} from '@material-ui/core';
 import useStyles from './styles.js';
+import {useState} from 'react';
 
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '', 
+        title: '', 
+        message: '',
+        tags: '', 
+        selectedFile: ''       
+    });
+
     const classes = useStyles();
 
     const handleSubmit = () => {
